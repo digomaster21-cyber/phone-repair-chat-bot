d3cb930098b7b0d157fcc6318ac61b303824d02e
digomaster21-cyber <digomaster21@gmail.com>
2025-12-27 18:21:06 +0300

    Add BUSINESS_INFO configuration for phone repair shop

---
+++ config.py
@@ -0,0 +1,24 @@
+
+BUSINESS_INFO = {
+    "name": "QuickFix Phone Repair",
+    "hours": "Monday to Saturday, 8am – 6pm",
+    "closed_days": ["Sunday"],
+    "phone": "(+255) 616627655",
+    "address": "154 makiyungu, singida, Tanzania",
+    "currency": "TZS",
+    "services": {
+        "screen": {
+            "name": "Screen Replacement",
+            "price": 25000,
+            "time": "1 hour",
+            "description": "Fix cracked or broken screens"
+        },
+        "battery": {
+            "name": "Battery Replacement",
+            "price": 15000,
+            "time": "45 minutes",
+            "description": "Replace old or failing batteries"
+        }
+    },
+    "warranty_days": 90
+}
